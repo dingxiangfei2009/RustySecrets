1627ce8e98247ba347cd2a2870a8b9f32a9dd3b3
Ding Xiang Fei <dingxiangfei2009@gmail.com>
2018-05-21 17:44:38 +0800

    wip

--- build.rs
+++ build.rs
@@ -70,14 +70,11 @@ impl fmt::Display for Tables {
 }
 
 fn build_protobuf<'a>(out_dir: &'a str, input: &'a [&'a str], includes: &'a [&'a str]) {
-    use self::protoc_rust::{run, Args, Customize};
+    use self::protoc_rust::{run, Args};
     run(Args {
         out_dir,
         input,
         includes,
-        customize: Customize {
-            ..Default::default()
-        },
     }).expect("protoc");
 }
 
